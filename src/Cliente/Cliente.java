912955d7e484ba6e2592ef318122817403d3d9a2
Marcos Souza <marcos.oliversouza@gmail.com>
2016-03-03 11:08:29 -0300

    Inicio do Projeto

---
+++ src/Cliente/Cliente.java
@@ -0,0 +1,20 @@
+package Cliente;
+
+import java.util.Observable;
+import java.util.Observer;
+
+public class Cliente implements Observer{
+	
+	Observable teste;
+	
+	@Override
+	public void update(Observable o, Object arg) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public static void main(String[] args) {  
+		// TODO Main
+	}
+	
+}
