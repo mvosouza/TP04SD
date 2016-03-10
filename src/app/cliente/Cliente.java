2c7d591c4bebd5312016ea2085f61bb3a78fcef7
Marcos Souza <marcos.oliversouza@gmail.com>
2016-03-10 11:56:56 -0300

    Utilizando o arquivo Properties

--- src/app/cliente/Cliente.java
+++ src/app/cliente/Cliente.java
@@ -1,10 +1,14 @@
 package app.cliente;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.ObjectOutputStream;
+import java.net.ConnectException;
 import java.net.InetAddress;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.Properties;
 
 import app.Mensagem;
 import app.ObservablePattern.ClienteObserver;
@@ -14,9 +18,11 @@ public class Cliente {
 	
 	private TelaApp janela;
 	private ClienteListener listener;
+	private Properties prop;
 	
 	public Cliente(int porta) {
 		super();
+		this.setProp(getNewProp());
 		this.janela = new TelaApp();
 		this.listener = new ClienteListener(porta, janela);
 		Thread tJanela = new Thread(janela);
@@ -24,9 +30,33 @@ public class Cliente {
 		Thread tListener = new Thread(listener);
 		tListener.start();
 		//conectando com o server
-		conexaoServer("200.239.139.205");
+		conexaoServer(prop.getProperty("primario"));
 	}
 	
+	public static Properties getNewProp() { 
+		Properties props = new Properties(); 
+		FileInputStream file;
+		try {
+			file = new FileInputStream( "config.properties");
+			props.load(file); 
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+		return props; 
+	}
+
+	public Properties getProp() {
+		return prop;
+	}
+
+	public void setProp(Properties prop) {
+		this.prop = prop;
+	}
+
 	public void conexaoServer(String ipServidor){
 		try {
 			//Porta do servidor 6000
@@ -37,8 +67,10 @@ public class Cliente {
 			out.close();
 			socket.close();
 			
+		} catch (ConnectException e) {
+			System.err.println("Error ao conectar com o Servidor "+ipServidor+" !");
+			conexaoServer(prop.getProperty("secundario"));
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
